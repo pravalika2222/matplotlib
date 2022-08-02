c961460baccc0c46b598720fe29d147311c39d0e
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-08-02 14:20:51 +0530

    Add files via upload

---
+++ matplotlib_program.py
@@ -0,0 +1,58 @@
+# import matplotlib.pyplot as plt
+# x=[10,20,30,40,50]
+# y=[0,2,4,6,8]
+# plt.bar(x,y,color='blue')
+# plt.show()                                             
+
+# import numpy as np
+# p=np.array(22)                                                        #  0  [] is not  there
+# q=np.array([1,2,3])                                                   #  1  []  1 is there
+# r=np.array([[1,2,3],[3,2,1]])                                         #  2  []  2 is there                             
+# s=np.array([[[1,2,3],[3,2,1]],[[4,5,6],[6,5,4]]])                     #  3  []  3 is there
+# print(p.ndim)
+# print(q.ndim)
+# print(r.ndim)
+# print(s.ndim)
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# m=np.array([1,6])
+# n=np.array([1,8])
+# plt.plot(m,n)
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# m=np.array([0,4])
+# n=np.array([2,6])
+# plt.plot(m,n,'o')                      #  o-circle
+# plt.plot(m,n,'s')                      #  s-square
+# plt.plot(m,n,'p')                      #  p-pentagon
+# plt.plot(m,n,'h')                      #  h-hexagon
+# plt.plot(m,n)                        # all the shapes are fromed on the line
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+# m=np.array([0,4])
+# n=np.array([2,6])
+# plt.plot(m,n)                                         # normal line
+# plt.plot(m,n,'p')                                    # it is a pentagon shape but it is without line
+# plt.plot(m,n,marker='p')                            # it is a pentagon shape but it is with line
+# plt.plot(m,n,'s:k')                                # it is a square shape and k means black colour and dotted line
+# plt.plot(m,n,'s:c')                               # it is a square shape and c - skyblue and  dotted line
+# plt.plot(m,n,'s:y')                              # it is a square shape and y - yellow and dotted line
+# plt.plot(m,n,'s:b')                             # it is  a square shape and b - blue and dotted line
+# plt.plot(m,n,'s:w')                            # it is plane 
+# plt.plot(m,n,'s-b')                           # it is a square shape and b - blue and plane line
+# plt.plot(m,n,'s--r')                         # it is a square shape and r-red colour and dotted line
+# plt.plot(m,n,'s--g')                        # it is a square shape and g-green and dotted line  
+# plt.plot(m,n,ls='dashed')                  # it is a dashed line
+# plt.plot(m,n,ls='dashdot')                # it is a dash and dot line
+# plt.plot(m,n,ls='dotted')                # it is a dotted line
+# plt.plot(m,n,ls='None')                 # it is a empty
+# plt.plot(m,n,ls='solid',lw='4')        # it is a thick line and it's linewidth(lw)  is  4
+# plt.plot(m,n,ls='solid',lw='2')       # it is a thick line and it's linewidth(lw) is 2
+# plt.show()
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
